addfb1d482d9765a31eb4463040bb2339eaa94de
Roberto - phproberto <roberto@phproberto.com>
2013-06-06 06:26:54 +0200

    [fixed][#RR-11] Add base table class

---
+++ libraries/redrad/base/table.php
@@ -0,0 +1,258 @@
+<?php
+/**
+ * @package     RedRad
+ * @subpackage  Base
+ *
+ * @copyright   Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+defined('JPATH_REDRAD') or die;
+
+/**
+ * redRAD Base Table
+ *
+ * @package     RedRad
+ * @subpackage  Base
+ * @since       1.0
+ */
+class RBaseTable extends JTable
+{
+	/**
+	 * Prefix to add to log files
+	 *
+	 * @var  string
+	 */
+	protected $_logPrefix = 'redrad';
+
+	/**
+	 * The table name without the prefix. Ex: cursos_courses
+	 *
+	 * @var  string
+	 */
+	protected $_tableName = null;
+
+	/**
+	 * The table key column. Usually: id
+	 *
+	 * @var  string
+	 */
+	protected $_tableKey = 'id';
+
+	/**
+	 * Field name to publish/unpublish/trash table registers. Ex: state
+	 *
+	 * @var  string
+	 */
+	protected $_tableFieldState = 'state';
+
+	/**
+	 * Constructor
+	 *
+	 * @param   JDatabase  &$db  A database connector object
+	 */
+	public function __construct(&$db)
+	{
+		// Keep checking _tbl value for standard defined tables
+		if (empty($this->_tbl) && !empty($this->_tableName))
+		{
+			// Add the table prefix
+			$this->_tbl = '#__' . $this->_tableName;
+		}
+
+		// Keep checking _tbl_key for standard defined tables
+		if (empty($this->_tbl_key) && !empty($this->_tableKey))
+		{
+			$this->_tbl_key = $this->_tableKey;
+		}
+
+		if (empty($this->_tbl) || empty($this->_tbl_key))
+		{
+			throw new UnexpectedValueException(sprintf('Missing data to initialize %s table | id: %s', $this->_tbl, $this->_tbl_key));
+			JLog::add(JText::sprintf('REDRAD_ERROR_INITIALIZING_TABLE', $this->_tbl, $this->_tbl_key), JLog::ERROR, $this->_logPrefix . 'Tables');
+
+			return false;
+		}
+
+		parent::__construct($this->_tbl, $this->_tbl_key, $db);
+	}
+
+	/**
+	 * Override the parent checkin method to set checked_out = null instead of 0 so the foreign key doesn't fail.
+	 *
+	 * @param   mixed  $pk  An optional primary key value to check out.  If not set the instance property value is used.
+	 *
+	 * @return  boolean  True on success.
+	 *
+	 * @throws  UnexpectedValueException
+	 */
+	public function checkIn($pk = null)
+	{
+		$k = $this->_tbl_key;
+		$pk = (is_null($pk)) ? $this->$k : $pk;
+
+		// If no primary key is given, return false.
+		if ($pk === null)
+		{
+			throw new UnexpectedValueException('Null primary key not allowed.');
+		}
+
+		// Check the row in by primary key.
+		$query = $this->_db->getQuery(true);
+		$query->update($this->_tbl);
+		$query->set($this->_db->quoteName('checked_out') . ' = NULL');
+		$query->set($this->_db->quoteName('checked_out_time') . ' = ' . $this->_db->quote($this->_db->getNullDate()));
+		$query->where($this->_tbl_key . ' = ' . $this->_db->quote($pk));
+		$this->_db->setQuery($query);
+
+		// Check for a database error.
+		$this->_db->execute();
+
+		// Set table values in the object.
+		$this->checked_out = null;
+		$this->checked_out_time = '';
+
+		return true;
+	}
+
+	/**
+	 * Method to set the publishing state for a row or list of rows in the database
+	 * table.  The method respects checked out rows by other users and will attempt
+	 * to checkin rows that it can after adjustments are made.
+	 *
+	 * @param   mixed    $pks     An optional array of primary key values to update.
+	 *                            If not set the instance property value is used.
+	 * @param   integer  $state   The publishing state. eg. [0 = unpublished, 1 = published]
+	 * @param   integer  $userId  The user id of the user performing the operation.
+	 *
+	 * @return  boolean  True on success; false if $pks is empty.
+	 */
+	public function publish($pks = null, $state = 1, $userId = 0)
+	{
+		// Use an easy to handle variable for database
+		$db = $this->_db;
+
+		// Initialise variables.
+		$k = $db->quoteName($this->_tbl_key);
+
+		// Sanitize input.
+		JArrayHelper::toInteger($pks);
+		$userId = (int) $userId;
+		$state  = (int) $state;
+
+		// If there are no primary keys set check to see if the instance key is set.
+		if (empty($pks))
+		{
+			if ($this->$k)
+			{
+				$pks = array($this->$k);
+			}
+
+			// Nothing to set publishing state on, return false.
+			else
+			{
+				$this->setError(JText::_('JLIB_DATABASE_ERROR_NO_ROWS_SELECTED'));
+
+				return false;
+			}
+		}
+
+		// Build the main update query
+		$query = $db->getQuery(true)
+				->update($db->quoteName($this->_tbl))
+				->set($db->quoteName($this->_tableFieldState) . ' = ' . (int) $state)
+				->where($k . '=' . implode(' OR ' . $k . '=', $pks));
+
+		// Determine if there is checkin support for the table.
+		if (!property_exists($this, 'checked_out') || !property_exists($this, 'checked_out_time'))
+		{
+			$checkin = false;
+		}
+		else
+		{
+			$query->where('(checked_out = 0 OR checked_out IS NULL OR checked_out = ' . (int) $userId . ')');
+			$checkin = true;
+		}
+
+		// Update the publishing state for rows with the given primary keys.
+		$db->setQuery($query);
+
+		try
+		{
+			$db->query();
+		}
+		catch (RuntimeException $e)
+		{
+			JError::raiseWarning(500, $e->getMessage());
+			JLog::add(JText::sprintf('REDRAD_ERROR_QUERY', $db->dump()), JLog::ERROR, $this->_logPrefix . 'Queries');
+
+			return false;
+		}
+
+		// If checkin is supported and all rows were adjusted, check them in.
+		if ($checkin && (count($pks) == $this->_db->getAffectedRows()))
+		{
+			// Checkin the rows.
+			foreach ($pks as $pk)
+			{
+				$this->checkin($pk);
+			}
+		}
+
+		// If the JTable instance value is in the list of primary keys that were set, set the instance.
+		if (in_array($this->$k, $pks))
+		{
+			$this->{$this->_tableFieldState} = $state;
+		}
+
+		$this->setError('');
+
+		return true;
+	}
+
+	/**
+	 * Delete on or more registers
+	 *
+	 * @param   string/array  $pk  Array of ids or ids comma separated
+	 *
+	 * @return  boolean            Deleted successfuly?
+	 */
+	public function delete($pk = null)
+	{
+		// Initialise variables.
+		$k = $this->_tbl_key;
+
+		// Received an array of ids?
+		if (is_array($pk))
+		{
+			// Sanitize input.
+			JArrayHelper::toInteger($pk);
+			$pk = implode(',', $pk);
+		}
+
+		$pk = (is_null($pk)) ? $this->$k : $pk;
+
+		// If no primary key is given, return false.
+		if ($pk === null)
+		{
+			return false;
+		}
+
+		// Delete the row by primary key.
+		$query = $this->_db->getQuery(true);
+		$query->delete($this->_db->quoteName($this->_tbl));
+		$query->where($this->_db->quoteName($this->_tbl_key) . ' IN (' . $this->_db->quote($pk) . ')');
+		$this->_db->setQuery($query);
+		$this->_db->query();
+
+		// Check for a database error.
+		if ($this->_db->getErrorNum())
+		{
+			$this->setError($this->_db->getErrorMsg());
+
+			return false;
+		}
+
+		return true;
+	}
+}
