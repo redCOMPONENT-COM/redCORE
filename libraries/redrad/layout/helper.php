8cdf7d2fc6bdc55a0b9edce9f91c1202efacd0aa
Roberto - phproberto <roberto@phproberto.com>
2013-06-06 06:00:15 +0200

    [fix][RR-13] Add layout class

---
+++ libraries/redrad/layout/helper.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * @package     RedRad
+ * @subpackage  Layout
+ *
+ * @copyright   Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+defined('JPATH_REDRAD') or die;
+
+/**
+ * Layout helper library
+ *
+ * @package     RedRad
+ * @subpackage  Layout
+ * @since       1.0
+ */
+class RLayoutHelper
+{
+	/**
+	 * Method to render the layout.
+	 *
+	 * @param   string  $layoutFile   Dot separated path to the layout file, relative to base path
+	 * @param   object  $displayData  Object which properties are used inside the layout file to build displayed output
+	 * @param   string  $basePath     Base path to use when loading layout files
+	 * @param   string  $component    Name of the component to use as source for the layout
+	 * @param   mixed   $client       Client to search the layout (0, 'site' , 1, 'admin')
+	 *
+	 * @return  string
+	 *
+	 * @since   3.1
+	 */
+	public static function render($layoutFile, $displayData = null, $basePath = '', $component = 'auto', $client = 'auto')
+	{
+		$basePath = empty($basePath) ? null : $basePath;
+
+		// Make sure we send null to JLayoutFile if no path set
+		$layout = new RLayout($layoutFile, $component, $client);
+
+		if (!empty($basePath))
+		{
+			$layout->addIncludePaths($basePath);
+		}
+
+		$renderedLayout = $layout->render($displayData);
+
+		return $renderedLayout;
+	}
+}
---
+++ libraries/redrad/layout/layout.php
@@ -0,0 +1,192 @@
+<?php
+/**
+ * @package     RedRad
+ * @subpackage  Layout
+ *
+ * @copyright   Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+defined('JPATH_REDRAD') or die;
+
+/**
+ * Layout library
+ *
+ * @package     RedRad
+ * @subpackage  Layout
+ * @since       1.0
+ */
+class RLayout extends JLayoutFile
+{
+	/**
+	 * Paths to search for layouts
+	 *
+	 * @var  array
+	 */
+	protected $availablePaths = array();
+
+	/**
+	 * Client 0 = site | 1 = admin
+	 *
+	 * @var  integer
+	 */
+	protected $client = 0;
+
+	/**
+	 * Method to instantiate the file-based layout.
+	 *
+	 * @param   string  $layoutId   Dot separated path to the layout file, relative to base path
+	 * @param   string  $component  Active component. Use null to skip it
+	 * @param   string  $client     site or 0 for frontend | anything else for backend
+	 *
+	 * @since   3.0
+	 */
+	public function __construct($layoutId, $component = 'auto', $client = 'auto')
+	{
+		// Autodetect component ?
+		if ($component == 'auto')
+		{
+			$component = JFactory::getApplication()->input->get('option', null);
+		}
+
+		$this->initClient($client);
+
+		$this->layoutId = $layoutId;
+
+		// Layouts relative to current component
+		if ($component)
+		{
+			// (1) Component template overrides path
+			$basePaths[] = JPATH_THEMES . '/' . JFactory::getApplication()->getTemplate() . '/html/layouts/' . $component;
+
+			// (2) Component path
+			if ($this->client == 0)
+			{
+				$basePaths[] = JPATH_SITE . '/components/' . $component . '/layouts';
+			}
+			else
+			{
+				$basePaths[] = JPATH_ADMINISTRATOR . '/components/' . $component . '/layouts';
+			}
+
+			// (3) Search a component library created like /libraries/redshop
+			$compNameParts = explode('_', $component);
+
+			if (count($compNameParts) == 2)
+			{
+				$basePaths[] = JPATH_LIBRARIES . '/' . $compNameParts[1] . '/layouts';
+			}
+		}
+
+		// (4) Library path
+		$this->addIncludePaths(JPATH_LIBRARIES . '/redrad/layouts');
+
+		// (5) Standard Joomla! layouts overriden
+		$basePaths[] = JPATH_THEMES . '/' . JFactory::getApplication()->getTemplate() . '/html/layouts';
+
+		// (6) Standard Joomla! layouts
+		$basePaths[] = $this->addIncludePaths(JPATH_ROOT . '/layouts');
+
+		$this->addIncludePaths($basePaths);
+	}
+
+	/**
+	 * Add one or more paths to include in layout search
+	 *
+	 * @param   string  $paths  The path or array of paths to search for layouts
+	 *
+	 * @return  void
+	 */
+	public function addIncludePaths($paths)
+	{
+		if (is_array($paths) && !empty($paths))
+		{
+			$this->availablePaths = array_merge($paths, $this->availablePaths);
+		}
+		else
+		{
+			array_unshift($this->availablePaths, $paths);
+		}
+	}
+
+	/**
+	 * Method to finds the full real file path, checking possible overrides
+	 *
+	 * @return  string  The full path to the layout file
+	 *
+	 * @since   3.0
+	 */
+	protected function getPath()
+	{
+		if (is_null($this->fullPath) && !empty($this->layoutId))
+		{
+			$rawPath  = str_replace('.', '/', $this->layoutId) . '.php';
+
+			$this->fullPath = JPath::find($this->availablePaths, $rawPath);
+		}
+
+		return $this->fullPath;
+	}
+
+	/**
+	 * Function to initialise the application client
+	 *
+	 * @param   mixed  $client  Frontend: 'site' or 0 | Backend: 'admin' or 1
+	 *
+	 * @return  void
+	 */
+	protected function initClient($client = 'auto')
+	{
+		// Force string conversion to avoid unexpected states
+		switch ((string) $client)
+		{
+			case 'site':
+			case '0':
+				$this->client = 0;
+				break;
+
+			case 'admin':
+			case '1':
+				$this->client = 1;
+				break;
+
+			default:
+				$this->client = (int) JFactory::getApplication()->isAdmin();
+				break;
+		}
+
+		return $this->client;
+	}
+
+	/**
+	 * Dirty function to debug layout path errors
+	 *
+	 * @return  void
+	 */
+	public function debugPath()
+	{
+		echo "<pre>";
+		print_r($this->availablePaths);
+		$rawPath  = str_replace('.', '/', $this->layoutId) . '.php';
+		echo 'Layout: ' . $rawPath . '<br>';
+
+		if ($this->availablePaths)
+		{
+			foreach ($this->availablePaths as $path)
+			{
+				$file = $path . '/' . $rawPath;
+
+				if (!file_exists($file))
+				{
+					echo '!exists: ' . $file . '<br />';
+				}
+				else
+				{
+					echo 'exists: ' . $file . '<br />';
+					break;
+				}
+			}
+		}
+		echo "</pre>";
+	}
+}
