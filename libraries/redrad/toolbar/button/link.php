7cca8613d96183996c3383b8ce7cc60ad8dd62c8
flo <voutzinos.florian@gmail.com>
2013-06-07 17:31:16 +0200

    [+] Add link button

---
+++ libraries/redrad/toolbar/button/link.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * @package     RedRad
+ * @subpackage  Toolbar
+ *
+ * @copyright   Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+defined('JPATH_REDRAD') or die;
+
+/**
+ * Represents a link button.
+ *
+ * @package     RedRad
+ * @subpackage  Toolbar
+ * @since       1.0
+ */
+class RToolbarButtonLink extends RToolbarButton
+{
+	/**
+	 * The button url.
+	 *
+	 * @var  string
+	 */
+	protected $url;
+
+	/**
+	 * Constructor.
+	 *
+	 * @param   string  $text       The button text.
+	 * @param   string  $url        The button task.
+	 * @param   string  $class      The button class.
+	 * @param   string  $iconClass  The icon class.
+	 */
+	public function __construct($text = '', $url = '', $class = '', $iconClass = '')
+	{
+		parent::__construct($text, $iconClass, $class);
+
+		$this->url = $url;
+	}
+
+	/**
+	 * Get the button url.
+	 *
+	 * @return  string  The url.
+	 */
+	public function getUrl()
+	{
+		return $this->url;
+	}
+
+	/**
+	 * Render the button.
+	 *
+	 * @return  string  The rendered button.
+	 */
+	public function render()
+	{
+		return RLayoutHelper::render('toolbar.button.link', array('button' => $this));
+	}
+}
