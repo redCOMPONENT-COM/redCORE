e3956d9634df3841dba5376c718ed7f4758468aa
flo <voutzinos.florian@gmail.com>
2013-06-10 14:48:52 +0200

    Add default controller admin

---
+++ libraries/redrad/controller/admin.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * @package     RedRad
+ * @subpackage  Controller
+ *
+ * @copyright   Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+defined('JPATH_REDRAD') or die;
+
+/**
+ * Controller Admin class.
+ *
+ * @package     RedRad
+ * @subpackage  Controller
+ * @since       1.0
+ */
+class RControllerAdmin extends JControllerAdmin
+{
+	/**
+	 * Method to get a model object, loading it if required.
+	 *
+	 * @param   string  $name    The model name. Optional.
+	 * @param   string  $prefix  The class prefix. Optional.
+	 * @param   array   $config  Configuration array for model. Optional.
+	 *
+	 * @return  object  The model.
+	 */
+	public function getModel($name = '', $prefix = '', $config = array('ignore_request' => true))
+	{
+		$class = get_class($this);
+
+		if (empty($name))
+		{
+			$name = strstr($class, 'Controller');
+			$name = str_replace('Controller', '', $name);
+			$name = \Doctrine\Common\Inflector\Inflector::singularize($name);
+		}
+
+		if (empty($prefix))
+		{
+			$prefix = strstr($class, 'Controller', true) . 'Model';
+		}
+
+		return parent::getModel($name, $prefix, $config);
+	}
+
+	/**
+	 * Method to save the submitted ordering values for records via AJAX.
+	 *
+	 * @return	void
+	 */
+	public function saveOrderAjax()
+	{
+		// Get the input
+		$pks   = $this->input->post->get('cid', array(), 'array');
+		$order = $this->input->post->get('order', array(), 'array');
+
+		// Sanitize the input
+		JArrayHelper::toInteger($pks);
+		JArrayHelper::toInteger($order);
+
+		// Get the model
+		$model = $this->getModel();
+
+		// Save the ordering
+		$return = $model->saveorder($pks, $order);
+
+		if ($return)
+		{
+			echo "1";
+		}
+
+		// Close the application
+		JFactory::getApplication()->close();
+	}
+}
