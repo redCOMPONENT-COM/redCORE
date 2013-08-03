7943d09f502a535c542ceef0c6ebff8562267b49
Matias Aguirre <maguirre@matware.com.ar>
2013-08-03 00:11:43 -0300

    Small fix loading classes

---
+++ libraries/redrad/oauth2/credentials/signer/plaintext.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * @package     RedRad
+ * @subpackage  OAuth2
+ *
+ * This work is based on a Louis Landry work about oauth1 server suport for Joomla! Platform.
+ * URL: https://github.com/LouisLandry/joomla-platform/tree/9bc988185ccc3e1c437256cc2c927e49312b3d00/libraries/joomla/oauth1
+ *
+ * @copyright   Copyright (C) 2012 - 2013 redCOMPONENT.com. All rights reserved.
+ * @license     GNU General Public License version 2 or later, see LICENSE.
+ */
+
+defined('JPATH_PLATFORM') or die;
+
+/**
+ * OAuth PLAINTEXT Signature Method class for the RedRAD.
+ *
+ * @package     RedRAD
+ * @subpackage  OAuth2
+ * @since       1.0
+ */
+class ROAuth2MessageSignerPlaintext extends ROAuth2MessageSigner
+{
+	/**
+	 * Calculate and return the OAuth message signature using PLAINTEXT
+	 *
+	 * @param   string  $baseString        The OAuth message as a normalized base string.
+	 * @param   string  $clientSecret      The OAuth client's secret.
+	 * @param   string  $credentialSecret  The OAuth credentials' secret.
+	 *
+	 * @return  string  The OAuth message signature.
+	 *
+	 * @since   1.0
+	 * @throws  InvalidArgumentException
+	 */
+	public function sign($baseString, $clientSecret, $credentialSecret)
+	{
+		return $clientSecret . '&' . $credentialSecret;
+	}
+
+	/**
+	 * Calculate and return the OAuth message signature using PLAINTEXT
+	 *
+	 * @param   string  $baseString        The OAuth message as a normalized base string.
+	 * @param   string  $clientSecret      The OAuth client's secret.
+	 * @param   string  $credentialSecret  The OAuth credentials' secret.
+	 *
+	 * @return  string  The OAuth message signature.
+	 *
+	 * @since   1.0
+	 * @throws  InvalidArgumentException
+	 */
+	public function clientDecode($clientSecret)
+	{
+		return explode(":", base64_decode($clientSecret))[1];
+	}
+}
