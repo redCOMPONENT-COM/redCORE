1adbec778a85c6077e394efaef0a33a24dc27486
Roberto - phproberto <roberto@phproberto.com>
2013-08-14 18:35:02 +0200

    Different JFormRule test call in 2.5 & 3.x

--- libraries/redrad/form/rules/date.php
+++ libraries/redrad/form/rules/date.php
@@ -16,7 +16,7 @@ defined('JPATH_REDRAD') or die;
  * @subpackage  Rules
  * @since       1.0
  */
-class JFormRuleDate extends JFormRule
+class JFormRuleDate extends RFormRule
 {
 	/**
 	 * Method to test if two values are not equal. To use this rule, the form
