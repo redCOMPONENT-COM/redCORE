28498e8a21f483fa2ef5ad49a15c2aaae2afa773
florianv <voutzinos.florian@gmail.com>
2013-09-06 00:30:05 +0200

    Fix search layout 3nd round

--- libraries/redrad/layouts/search.php
+++ libraries/redrad/layouts/search.php
@@ -28,20 +28,27 @@ else
 {
 	throw new InvalidArgumentException('No View passed to the "search" layout.');
 }
+
+$filterName = 'search';
+
+if (isset($data['filter_name']))
+{
+	$filterName = $data['filter_name'];
+}
 ?>
 
 <div id="filter-bar" class="btn-toolbar">
 	<div class="filter-search btn-group pull-left">
-		<input type="text" name="filter_search" id="filter_search"
+		<input type="text" name="filter_<?php echo $filterName ?>" id="filter_<?php echo $filterName ?>"
 		       placeholder="<?php echo JText::_('JSEARCH'); ?>"
-		       value="<?php echo $state->get('filter.search'); ?>"
+		       value="<?php echo $state->get('filter.' . $filterName); ?>"
 		       title="<?php echo JText::_('JSEARCH'); ?>"/>
 	</div>
 	<div class="btn-group hidden-phone">
 		<button class="btn hasTooltip" type="submit" title="<?php echo JText::_('JSEARCH_FILTER_SUBMIT'); ?>">
 			<i class="icon-search"></i>
 		</button>
-		<button class="btn hasTooltip" type="button" onclick="document.id('filter_search').value='';this.form.submit();"
+		<button class="btn hasTooltip" type="button" onclick="document.id('filter_<?php echo $filterName ?>').value='';this.form.submit();"
 		        title="<?php echo JText::_('JSEARCH_FILTER_CLEAR'); ?>">
 			<i class="icon-remove"></i>
 		</button>
