659e3fecd610dee628839f69b9b32d0b9ae3ebba
Roberto - phproberto <roberto@phproberto.com>
2013-10-17 10:27:51 +0200

    [fix] rbootstrap not found because JFormHelper is adding include paths to JHtml not RHtml

--- libraries/redcore/layouts/component/site.php
+++ libraries/redcore/layouts/component/site.php
@@ -66,61 +66,6 @@ if (version_compare(JVERSION, '3.0', '<') && JFactory::getApplication()->isAdmin
 	}
 }
 
-// Do we have to display the sidebar ?
-$displaySidebar = false;
-
-if (isset($data['sidebar_display']))
-{
-	$displaySidebar = (bool) $data['sidebar_display'];
-}
-
-$sidebarLayout = '';
-
-// The sidebar layout name.
-if ($displaySidebar)
-{
-	if (!isset($data['sidebar_layout']))
-	{
-		throw new InvalidArgumentException('No sidebar layout specified in the component layout.');
-	}
-
-	$sidebarLayout = $data['sidebar_layout'];
-}
-
-$sidebarData = array();
-
-if (isset($data['sidebar_data']))
-{
-	$sidebarData = $data['sidebar_data'];
-}
-
-// Do we have to display the topbar ?
-$displayTopbar = false;
-
-if (isset($data['topbar_display']))
-{
-	$displayTopbar = (bool) $data['topbar_display'];
-}
-
-$topbarLayout = '';
-
-// The topbar layout name.
-if ($displayTopbar)
-{
-	if (!isset($data['topbar_layout']))
-	{
-		throw new InvalidArgumentException('No topbar layout specified in the component layout.');
-	}
-
-	$topbarLayout = $data['topbar_layout'];
-}
-
-$topbarData = array();
-
-if (isset($displayTopbar))
-{
-	$topbarData = $data;
-}
 
 // The view to render.
 if (!isset($data['view']))
@@ -140,8 +85,6 @@ if (!$view instanceof RViewBase)
 		)
 	);
 }
-
-$toolbar = $view->getToolbar();
 
 // Get the view template.
 $tpl = $data['tpl'];
