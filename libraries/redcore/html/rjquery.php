128f398f59324326e270ae4b9928f0ef8cc93425
Kixo <kristijan.zivcec@gmail.com>
2014-06-10 07:31:54 +0200

    jQuery_always_first_fix_Thongs_code

--- libraries/redcore/html/rjquery.php
+++ libraries/redcore/html/rjquery.php
@@ -163,8 +163,17 @@ abstract class JHtmlRjquery
 		// Load jQuery in administration, or if it's frontend site and it has been asked via plugin parameters
 		if ($isAdmin || (!$isAdmin && RBootstrap::$loadFrontendjQuery))
 		{
-			RHelperAsset::load('lib/jquery.min.js', self::EXTENSION);
-			RHelperAsset::load('lib/jquery-noconflict.js', self::EXTENSION);
+			$doc = JFactory::getDocument();
+
+			$jqueryLib = array(
+				JUri::root(true) . '/media/redcore/js/lib/jquery.min.js' => array('mime' => 'text/javascript', 'defer' => '', 'async' => '')
+			);
+			$jqueryNoConflict = array(
+				JUri::root(true) . '/media/redcore/js/lib/jquery-noconflict.js' => array('mime' => 'text/javascript', 'defer' => '', 'async' => '')
+			);
+
+			$doc->_scripts = array_merge($jqueryNoConflict, $doc->_scripts);
+			$doc->_scripts = array_merge($jqueryLib, $doc->_scripts);
 		}
 		elseif (!$isAdmin && !RBootstrap::$loadFrontendBootstrap && !version_compare(JVERSION, '3.0', '<'))
 		{
