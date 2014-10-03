45c3d0373680f7242ae1a9d1aba0d628e926872e
Kixo <kristijan.zivcec@gmail.com>
2014-10-03 17:37:48 +0200

    added separation for administrator webservices and site webservices fixed buttons in administration / added documentation button in admin added com_contacts for frontend as webservice etc...

--- libraries/redcore/api/hal/document/base.php
+++ libraries/redcore/api/hal/document/base.php
@@ -27,7 +27,7 @@ abstract class RApiHalDocumentBase
 	 * @param   SimpleXMLElement     $xml   XML document
 	 * @param   RApiHalDocumentLink  $link  Link element
 	 *
-	 * @return AbstractHal
+	 * @return RApiHalDocumentBase
 	 */
 	public function setXMLAttributes(SimpleXMLElement $xml, RApiHalDocumentLink $link)
 	{
