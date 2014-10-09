32967226b6d9ff0e238405f0a29d6e4f14fba3e7
javier gomez <javier.gomez.corio@gmail.com>
2014-10-09 13:53:51 +0200

    Ignore code style checks in Tables

--- .travis/phpcs.php
+++ .travis/phpcs.php
@@ -35,6 +35,7 @@ $ignored = array(
 	REPO_BASE . '/libraries/redcore/model/admin.php',
 	REPO_BASE . '/libraries/redcore/oauth/*',
 	REPO_BASE . '/libraries/redcore/oauth2/*',
+	REPO_BASE . '/libraries/redcore/table/*',
 	REPO_BASE . '/modules/site/mod_redcore_language_switcher/tmpl/*'
 );
 
