aacdc4dedd0ba79171d2d264d0c38dc8cd13b5f9
javier gomez <javier.gomez.corio@gmail.com>
2014-10-09 14:26:27 +0200

    Ignoring rgrid  due to it's long methods
    FILE: .../XAMPP/xamppfiles/htdocs/repos/redcore/libraries/redcore/html/rgrid.php
    --------------------------------------------------------------------------------
    FOUND 10 ERROR(S) AFFECTING 5 LINE(S)
    --------------------------------------------------------------------------------
      94 | ERROR | Multi-line function declaration not indented correctly; expected
         |       | 2 spaces but found 31
      94 | ERROR | Tabs must be used to indent lines; spaces are not allowed
      95 | ERROR | Multi-line function declaration not indented correctly; expected
         |       | 2 spaces but found 31
      95 | ERROR | Tabs must be used to indent lines; spaces are not allowed
     180 | ERROR | Multi-line function declaration not indented correctly; expected
         |       | 2 spaces but found 30
     180 | ERROR | Tabs must be used to indent lines; spaces are not allowed
     221 | ERROR | Multi-line function declaration not indented correctly; expected
         |       | 2 spaces but found 34
     221 | ERROR | Tabs must be used to indent lines; spaces are not allowed
     443 | ERROR | Multi-line function declaration not indented correctly; expected
         |       | 2 spaces but found 29
     443 | ERROR | Tabs must be used to indent lines; spaces are not allowed
    --------------------------------------------------------------------------------

--- .travis/phpcs.php
+++ .travis/phpcs.php
@@ -41,6 +41,7 @@ $ignored = array(
 	REPO_BASE . '/libraries/redcore/model/model.php',
 	REPO_BASE . '/libraries/redcore/modal/modal.php',
 	REPO_BASE . '/libraries/redcore/html/rsortablelist.php',
+	REPO_BASE . '/libraries/redcore/html/rgrid.php',
 	REPO_BASE . '/libraries/redcore/functions.php',
 	REPO_BASE . '/modules/site/mod_redcore_language_switcher/tmpl/*'
 );
