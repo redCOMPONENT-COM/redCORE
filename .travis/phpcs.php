43f99e1c328402f3e25c77c2ba51d4cbfb5ae3ec
Kixo <kristijan.zivcec@gmail.com>
2014-10-30 20:37:50 +0100

    REDCORE-224_fixing_codestyle

--- .travis/phpcs.php
+++ .travis/phpcs.php
@@ -48,6 +48,7 @@ $ignored = array(
 	REPO_BASE . '/libraries/redcore/database/driver.php',
 	REPO_BASE . '/libraries/redcore/table/*',
 	REPO_BASE . '/modules/site/mod_redcore_language_switcher/tmpl/*',
+	REPO_BASE . '/modules/site/mod_redcore_language_switcher/css/mod_redcore_language_switcher.css',
 );
 
 // Build the options for the sniffer
