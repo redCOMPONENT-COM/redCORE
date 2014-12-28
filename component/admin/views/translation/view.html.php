18d326c53ee5015c4f6ee9afb9e4a829044472a3
Kixo <kristijan.zivcec@gmail.com>
2014-12-28 21:31:41 +0100

    REDCORE-299_plugin_disabled_warning

--- component/admin/views/translation/view.html.php
+++ component/admin/views/translation/view.html.php
@@ -113,6 +113,17 @@ class RedcoreViewTranslation extends RedcoreHelpersView
 			}
 		}
 
+		// Check if option is enabled
+		if (RTranslationHelper::$pluginParams->get('enable_translations', 0) == 0)
+		{
+			JFactory::getApplication()->enqueueMessage(
+				JText::sprintf(
+					'COM_REDCORE_CONFIG_TRANSLATIONS_PLUGIN_LABEL_WARNING',
+					'<a href="index.php?option=com_plugins&view=plugins&filter_search=redcore">' . JText::_('COM_REDCORE_CONFIGURE') . '</a>'
+				),
+				'error');
+		}
+
 		parent::display($tpl);
 	}
 
--- component/admin/views/webservices/view.html.php
+++ component/admin/views/webservices/view.html.php
@@ -80,6 +80,17 @@ class RedcoreViewWebservices extends RedcoreHelpersView
 
 		$this->return = base64_encode('index.php?option=com_redcore&view=webservices');
 
+		// Check if option is enabled
+		if (RTranslationHelper::$pluginParams->get('enable_webservices', 0) == 0)
+		{
+			JFactory::getApplication()->enqueueMessage(
+				JText::sprintf(
+					'COM_REDCORE_WEBSERVICES_PLUGIN_LABEL_WARNING',
+					'<a href="index.php?option=com_plugins&view=plugins&filter_search=redcore">' . JText::_('COM_REDCORE_CONFIGURE') . '</a>'
+				),
+				'error');
+		}
+
 		parent::display($tpl);
 	}
 
