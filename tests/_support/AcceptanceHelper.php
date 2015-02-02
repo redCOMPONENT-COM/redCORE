f954871309095ecfe562a687496b5b0affdc93cc
puneet0191 <puneet.0191@gmail.com>
2015-02-02 22:21:52 +0530

    REDCORE-275 Adding new parameters to the config Array and Acceptance Helper

--- tests/_support/AcceptanceHelper.php
+++ tests/_support/AcceptanceHelper.php
@@ -40,7 +40,11 @@ class AcceptanceHelper extends \Codeception\Module
 		"site_name" => $this->config['site_name'],
 		"admin_email" => $this->config['admin_email'],
 		"language" => $this->config['language'],
-		"sample_data" => $this->config['sample_data']
+		"sample_data" => $this->config['sample_data'],
+		"host" => $this->config['host'],
+		"extension_name" => $this->config['extension_name'],
+		"install_extension_demo_data" => $this->config['install_extension_demo_data'],
+		"env" => $this->config['env']
 		];
 
 		return $configuration;
