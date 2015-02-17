9430935c9949e8dd6f3fdf593f95dfbae2653a6b
Kixo <kristijan.zivcec@gmail.com>
2015-02-17 23:14:58 +0100

    REDCORE-312_config_view_should_be_optional_bootstrap2

--- component/admin/layouts/sidebar.php
+++ component/admin/layouts/sidebar.php
@@ -3,7 +3,7 @@
  * @package     Redcore.Admin
  * @subpackage  Layouts
  *
- * @copyright   Copyright (C) 2008 - 2015 redCOMPONENT.com. All rights reserved.
+ * @copyright   Copyright (C) 2012 - 2015 redCOMPONENT.com. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE
  */
 
@@ -20,99 +20,90 @@ if (empty($return))
 	$return = base64_encode('index.php?option=com_redcore&view=dashboard');
 }
 ?>
-<div class="panel-group" id="rc-sidebar-accordion">
-	<div class="panel panel-default">
-		<div class="panel-heading">
-			<h4 class="panel-title">
-				<a href="<?php echo JRoute::_('index.php?option=com_redcore&view=dashboard') ?>">
-					<i class="icon-dashboard"></i>
-					<?php echo JText::_('COM_REDCORE_DASHBOARD') ?></a>
-			</h4>
-		</div>
-	</div>
-	<div class="panel panel-default">
-		<div class="panel-heading">
-			<h4 class="panel-title">
-				<a data-toggle="collapse" data-parent="#rc-sidebar-accordion" href="#rc-sidebar-accordion-configuration">
+<ul class="nav nav-list">
+	<?php if ($view === 'dashboards'): ?>
+		<li class="nav-header"><?php echo JText::_('COM_REDCORE_DASHBOARD') ?></li>
+	<?php else: ?>
+		<li>
+			<a href="<?php echo JRoute::_('index.php?option=com_redcore&view=dashboard') ?>">
+				<i class="icon-dashboard"></i>
+				<?php echo JText::_('COM_REDCORE_DASHBOARD') ?>
+			</a>
+		</li>
+	<?php endif; ?>
+
+	<?php if ($view === 'configs'): ?>
+		<li class="nav-header"><?php echo JText::_('COM_REDCORE_CONFIGURATION') ?></li>
+		<?php foreach ($components as $component) : ?>
+			<li class="<?php echo $option == $component->option ? 'active' : ''; ?>">
+				<a href="<?php echo JRoute::_('index.php?option=com_redcore&view=config&layout=edit&component=' . $component->option . '&return=' . $return) ?>">
 					<i class="icon-cogs"></i>
-					<?php echo JText::_('COM_REDCORE_CONFIGURATION') ?></a>
-			</h4>
-		</div>
-		<div id="rc-sidebar-accordion-configuration" class="panel-collapse collapse<?php echo $view === 'configs' ? ' in' : '';?>">
-			<ul class="list-group">
-				<?php foreach ($components as $component) : ?>
-					<li class="list-group-item <?php echo $option == $component->option ? 'list-group-item-info' : ''; ?>">
-						<a href="<?php echo JRoute::_('index.php?option=com_redcore&view=config&layout=edit&component=' . $component->option . '&return=' . $return) ?>">
-							<i class="icon-cogs"></i>
-							<?php echo JText::_($component->xml->name); ?>
-						</a>
-					</li>
-				<?php endforeach; ?>
-			</ul>
-		</div>
-	</div>
-	<div class="panel panel-default">
-		<div class="panel-heading">
-			<h4 class="panel-title">
-				<a
-					data-toggle="collapse"
-					data-parent="#rc-sidebar-accordion"
-					href="#rc-sidebar-accordion-translations">
-					<i class="icon-cogs"></i>
-					<?php echo JText::_('COM_REDCORE_TRANSLATIONS') ?></a>
-			</h4>
-		</div>
-		<div id="rc-sidebar-accordion-translations" class="panel-collapse collapse<?php echo $view === 'translations' ? ' in' : '';?>">
-			<ul class="list-group">
-				<li class="list-group-item">
-					<a href="<?php echo JRoute::_('index.php?option=com_redcore&view=translations&contentelement=&layout=manage&return=' . $return) ?>">
-						<i class="icon-globe"></i>
-						<?php echo JText::_('COM_REDCORE_TRANSLATIONS_MANAGE_CONTENT_ELEMENTS') ?>
-					</a>
-				</li>
-				<?php foreach ($translationTables as $translationTable) : ?>
-					<li class="list-group-item <?php echo $contentElement == str_replace('#__', '', $translationTable->table) ? 'list-group-item-info' : ''; ?>">
-						<a href="<?php echo JRoute::_(
-							'index.php?option=com_redcore&view=translations&component=' . $translationTable->option . '&contentelement='
-							. str_replace('#__', '', $translationTable->table)
-							. '&return=' . $return
-						); ?>">
-							<i class="icon-globe"></i>
-							<?php echo $translationTable->name; ?>
-						</a>
-					</li>
-				<?php endforeach; ?>
-			</ul>
-		</div>
-	</div>
-	<div class="panel panel-default">
-		<div class="panel-heading">
-			<h4 class="panel-title">
-				<a href="<?php echo JRoute::_('index.php?option=com_redcore&view=webservices') ?>">
+					<?php echo JText::_($component->xml->name); ?>
+				</a>
+			</li>
+		<?php endforeach; ?>
+		<li class="divider"></li>
+	<?php else: ?>
+		<li>
+			<a href="<?php echo JRoute::_('index.php?option=com_redcore&view=configs') ?>">
+				<i class="icon-cogs"></i>
+				<?php echo JText::_('COM_REDCORE_CONFIGURATION'); ?>
+			</a>
+		</li>
+	<?php endif; ?>
+
+	<?php if ($view === 'translations'): ?>
+		<li class="nav-header"><?php echo JText::_('COM_REDCORE_TRANSLATIONS') ?></li>
+		<?php foreach ($translationTables as $translationTable) : ?>
+
+			<li class="<?php echo $contentElement == str_replace('#__', '', $translationTable->table) ? 'active' : ''; ?>">
+				<a href="<?php echo JRoute::_(
+					'index.php?option=com_redcore&view=translations&component=' . $translationTable->option . '&contentelement='
+					. str_replace('#__', '', $translationTable->table)
+					. '&return=' . $return
+				); ?>">
 					<i class="icon-globe"></i>
-					<?php echo JText::_('COM_REDCORE_WEBSERVICES') ?>
+					<?php echo $translationTable->name; ?>
 				</a>
-			</h4>
-		</div>
-	</div>
-	<div class="panel panel-default">
-		<div class="panel-heading">
-			<h4 class="panel-title">
-				<a href="<?php echo JRoute::_('index.php?option=com_redcore&view=oauth_clients') ?>">
-					<i class="icon-globe"></i>
-					<?php echo JText::_('COM_REDCORE_OAUTH_CLIENTS') ?>
-				</a>
-			</h4>
-		</div>
-	</div>
-	<div class="panel panel-default">
-		<div class="panel-heading">
-			<h4 class="panel-title">
-				<a href="http://redcomponent-com.github.io/redCORE/" target="_blank">
-					<i class="icon-book"></i>
-					<?php echo JText::_('COM_REDCORE_DOCUMENTATION_LINK_TITLE') ?>
-				</a>
-			</h4>
-		</div>
-	</div>
-</div>
+			</li>
+		<?php endforeach; ?>
+		<li class="divider"></li>
+	<?php else: ?>
+		<li>
+			<a href="<?php echo JRoute::_('index.php?option=com_redcore&view=translations&contentelement=&layout=manage&return=' . $return) ?>">
+				<i class="icon-globe"></i>
+				<?php echo JText::_('COM_REDCORE_TRANSLATIONS') ?>
+			</a>
+		</li>
+	<?php endif; ?>
+
+	<?php if ($view === 'webservices'): ?>
+		<li class="nav-header"><?php echo JText::_('COM_REDCORE_WEBSERVICES') ?></li>
+		<li class="divider"></li>
+	<?php else: ?>
+		<li>
+			<a href="<?php echo JRoute::_('index.php?option=com_redcore&view=webservices') ?>">
+				<i class="icon-globe"></i>
+				<?php echo JText::_('COM_REDCORE_WEBSERVICES') ?>
+			</a>
+		</li>
+	<?php endif; ?>
+
+	<?php if ($view === 'oauth_clients'): ?>
+		<li class="nav-header"><?php echo JText::_('COM_REDCORE_OAUTH_CLIENTS') ?></li>
+		<li class="divider"></li>
+	<?php else: ?>
+		<li>
+			<a href="<?php echo JRoute::_('index.php?option=com_redcore&view=oauth_clients') ?>">
+				<i class="icon-globe"></i>
+				<?php echo JText::_('COM_REDCORE_OAUTH_CLIENTS') ?>
+			</a>
+		</li>
+	<?php endif; ?>
+	<li>
+		<a href="http://redcomponent-com.github.io/redCORE/" target="_blank">
+			<i class="icon-book"></i>
+			<?php echo JText::_('COM_REDCORE_DOCUMENTATION_LINK_TITLE') ?>
+		</a>
+	</li>
+</ul>
