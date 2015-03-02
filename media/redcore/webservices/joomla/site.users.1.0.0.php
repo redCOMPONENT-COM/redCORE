5a3003abd3f4a0245d71e00d86fb3ca79e4f286c
thongredweb <thong@redweb.dk>
2015-03-02 17:53:49 +0700

    NAA-348 #Comment Add remindUsername service for com_users

--- media/redcore/webservices/joomla/site.users.1.0.0.php
+++ media/redcore/webservices/joomla/site.users.1.0.0.php
@@ -18,6 +18,13 @@ defined('JPATH_BASE') or die;
  */
 class RApiHalHelperSiteUsers
 {
+	/**
+	 * Service for reset password of user
+	 *
+	 * @param   string  $email  Email of user account
+	 *
+	 * @return  boolean         True on success. False otherwise.
+	 */
 	public function forgotPassword($email)
 	{
 		// Load language from com_users
@@ -31,11 +38,40 @@ class RApiHalHelperSiteUsers
 		JForm::addFieldPath(JPATH_SITE . '/components/com_users/models/fields');
 		JLoader::import('route', JPATH_SITE . '/components/com_users/helpers');
 
-		$resetModel = RModel::getFrontInstance('Reset', array('ignore_request' => true), 'com_users');
-		$data = array('email' => $email);
+		$model = RModel::getFrontInstance('Reset', array('ignore_request' => true), 'com_users');
+		$data  = array('email' => $email);
 
 		// Submit the password reset request.
-		$return	= $resetModel->processResetRequest($data);
+		$return	= $model->processResetRequest($data);
+
+		return (boolean) $return;
+	}
+
+	/**
+	 * Service for remind username of user
+	 *
+	 * @param   string  $email  Email of user account
+	 *
+	 * @return  boolean         True on success. False otherwise.
+	 */
+	public function remindUsername($email)
+	{
+		// Load language from com_users
+		$language = JFactory::getLanguage();
+		$language->load('com_users');
+
+		// Load stuff from com_users
+		jimport('joomla.application.component.model');
+		JModelLegacy::addIncludePath(JPATH_SITE . '/components/com_users/models');
+		JForm::addFormPath(JPATH_SITE . '/components/com_users/models/forms');
+		JForm::addFieldPath(JPATH_SITE . '/components/com_users/models/fields');
+		JLoader::import('route', JPATH_SITE . '/components/com_users/helpers');
+
+		$model = RModel::getFrontInstance('Remind', array('ignore_request' => true), 'com_users');
+		$data  = array('email' => $email);
+
+		// Submit the password reset request.
+		$return	= $model->processRemindRequest($data);
 
 		return (boolean) $return;
 	}
