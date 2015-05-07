163280ad6f1ad245d67a49890d16425a49eef18a
javier gomez <javier.gomez.corio@gmail.com>
2015-05-07 17:43:58 +0200

    Use robo from composer and remove the .phar

--- RoboFile.php
+++ RoboFile.php
@@ -2,6 +2,9 @@
 /**
  * This is project's console commands configuration for Robo task runner.
  *
+ * Download robo.phar from http://robo.li/robo.phar and type in the root of the repo: $ php robo.phar
+ * Or do: $ composer update, and afterwards you will be able to execute robo like $ php vendor/bin/robo
+ *
  * @see http://robo.li/
  */
 require_once 'vendor/autoload.php';
