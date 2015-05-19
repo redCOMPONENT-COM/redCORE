857783470f43a7bdf2fdc6438d30b82e55136d82
Tito Alvarez <augustoalvarez@gmail.com>
2015-05-19 07:38:26 -0600

    REDCORE-398 Optimization of composer inclusion and selenium log

--- RoboFile.php
+++ RoboFile.php
@@ -110,7 +110,7 @@ class RoboFile extends \Robo\Tasks
         $this->taskComposerUpdate()->run();
 
         // Running Selenium server
-        $this->_exec("java -jar $seleniumPath > selenium-errors.log 2>selenium.log &");
+        $this->_exec("java -jar $seleniumPath >> selenium.log 2>&1 &");
 
         $this->taskWaitForSeleniumStandaloneServer()
             ->run()
@@ -127,23 +127,15 @@ class RoboFile extends \Robo\Tasks
         // $this->_exec('curl http://localhost:4444/selenium-server/driver/?cmd=shutDownSeleniumServer');
 
         $this->say('Printing Selenium Log files');
-        $this->say('------ selenium-errors.log (start) ---------');
-        $seleniumErrors = file_get_contents('selenium-errors.log');
+        $this->say('------ selenium.log (start) ---------');
+        $seleniumErrors = file_get_contents('selenium.log');
         if ($seleniumErrors) {
-            $this->say(file_get_contents('selenium-errors.log'));
+            $this->say(file_get_contents('selenium.log'));
         }
         else {
             $this->say('no errors were found');
         }
-        $this->say('------ selenium-errors.log (end) -----------');
-
-        /*
-        // Uncomment if you need to debug issues in selenium
-        $this->say('');
-        $this->say('------ selenium.log (start) -----------');
-        $this->say(file_get_contents('selenium.log'));
         $this->say('------ selenium.log (end) -----------');
-        */
     }
 
     /**
