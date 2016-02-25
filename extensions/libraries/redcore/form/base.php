5bc777879700f386f8f04ae039eeb2cc011cefc6
Kixo <kristijan.zivcec@gmail.com>
2016-02-25 08:30:08 +0100

    REDCORE-457_update_for_joomla_3_5

--- extensions/libraries/redcore/form/base.php
+++ extensions/libraries/redcore/form/base.php
@@ -74,6 +74,80 @@ abstract class RFormBase extends JForm
 	}
 
 	/**
+	 * Override the validate() method to store the error per field.
+	 *
+	 * @param   array   $data   An array of field values to validate.
+	 * @param   string  $group  The optional dot-separated form group path on which to filter the
+	 *                          fields to be validated.
+	 *
+	 * @return  mixed  True on sucess.
+	 */
+	public function validate($data, $group = null)
+	{
+		// Make sure there is a valid JForm XML document.
+		if (!($this->xml instanceof SimpleXMLElement))
+		{
+			return false;
+		}
+
+		$return = true;
+
+		// Create an input registry object from the data to validate.
+		$input = new JRegistry($data);
+
+		// Get the fields for which to validate the data.
+		$fields = $this->findFieldsByGroup($group);
+
+		if (!$fields)
+		{
+			// PANIC!
+			return false;
+		}
+
+		/** @var $field SimpleXMLElement */
+		foreach ($fields as $field)
+		{
+			$value = null;
+			$name = (string) $field['name'];
+
+			// Get the group names as strings for ancestor fields elements.
+			$attrs = $field->xpath('ancestor::fields[@name]/@name');
+			$groups = array_map('strval', $attrs ? $attrs : array());
+			$group = implode('.', $groups);
+
+			// Get the value from the input data.
+			if ($group)
+			{
+				$value = $input->get($group . '.' . $name);
+			}
+			else
+			{
+				$value = $input->get($name);
+			}
+
+			// Validate the field.
+			$valid = $this->validateField($field, $group, $value, $input);
+
+			// Check for an error.
+			if ($valid instanceof Exception)
+			{
+				if ($group)
+				{
+					$this->errors[$group . '.' . $name] = $valid;
+				}
+				else
+				{
+					$this->errors[$name] = $valid;
+				}
+
+				$return = false;
+			}
+		}
+
+		return $return;
+	}
+
+	/**
 	 * Method to get the error for a field input.
 	 *
 	 * @param   string  $name   The name of the form field.
