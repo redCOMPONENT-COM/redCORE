33ed9eae883d8464e6f1fe0701f1dcb0444bada9
Kristijan Zivcec <kristijan.zivcec@gmail.com>
2016-04-14 23:03:44 +0200

    REDCORE-464_problem_with_select_user_in_joomla_3

--- extensions/components/com_redcore/admin/updates/1.8.3.php
+++ extensions/components/com_redcore/admin/updates/1.8.3.php
@@ -75,35 +75,4 @@ class Com_RedcoreUpdateScript_1_8_3
 
 		return (array) $component->params->get('translations', array());
 	}
-
-	/**
-	 * Performs the upgrade after initial Joomla update for this version
-	 *
-	 * @param   JInstallerAdapter  $parent  Class calling this method
-	 *
-	 * @return  bool
-	 */
-	public function execute($parent)
-	{
-		// Clean old library files because we moved them to the extension folder
-		$files = array(
-			JPATH_ADMINISTRATOR . '/language/en-GB/en-GB.com_redcore.ini',
-			JPATH_ADMINISTRATOR . '/language/en-GB/en-GB.com_redcore.sys.ini',
-			JPATH_ADMINISTRATOR . '/language/da-DK/da-DK.com_redcore.ini',
-			JPATH_ADMINISTRATOR . '/language/da-DK/da-DK.com_redcore.sys.ini',
-		);
-
-		if (!empty($files))
-		{
-			foreach ($files as $path)
-			{
-				if (JFile::exists($path))
-				{
-					JFile::delete($path);
-				}
-			}
-		}
-
-		return true;
-	}
 }
