7e03935c9b9e0af91c506a933febdb3a7087d79f
Tito Alvarez <augustoalvarez@gmail.com>
2016-07-28 09:52:38 -0600

    Improved test compatibility with different PHP versions

--- tests/travis-php-fpm.sh
+++ tests/travis-php-fpm.sh
@@ -2,7 +2,8 @@
 
 file="$1"
 owner="$2"
-sed -e "s,listen = 127.0.0.1:9000,listen = /tmp/php5-fpm.sock,g" --in-place $file
+phpversion="$3"
+sed -e "s,listen = 127.0.0.1:9000,listen = /tmp/php$phpversion-fpm.sock,g" --in-place $file
 sed -e "s,;listen.owner = nobody,listen.owner = $owner,g" --in-place $file
 sed -e "s,;listen.group = nobody,listen.group = $owner,g" --in-place $file
 sed -e "s,;listen.mode = 0660,listen.mode = 0666,g" --in-place $file
