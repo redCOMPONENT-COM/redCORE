7bc26a1461d7a4dca1a2a40613987f3395b93edb
nikolagajski <nikola.gajski01@gmail.com>
2017-06-20 11:46:20 +0200

    scrutinizer_issue_1

--- extensions/libraries/redcore/api/hal/helper.php
+++ extensions/libraries/redcore/api/hal/helper.php
@@ -185,7 +185,7 @@ class RApiHalHelper
 		if (strpos($command, '.') !== false)
 		{
 			// Explode the controller.task command.
-			list ($type, $task) = explode('.', $command);
+			list ($task) = explode('.', $command);
 		}
 		else
 		{
