ac1c7cac70288d33821a69006bce3d2f8cf0d82d
João Reys Santos <je_reyssantos@msn.com>
2017-08-03 19:55:49 +0100

    Adding layout for bootstrap2 and 3

--- extensions/libraries/redcore/layouts/modal/dialog.php
+++ extensions/libraries/redcore/layouts/modal/dialog.php
@@ -5,19 +5,15 @@ defined('_JEXEC') or die;
 extract($displayData);
 ?>
 
-<div class="modal fade" id="<?php echo $selector; ?>" tabindex="-1" role="dialog" aria-hidden="true">
-	<div class="modal-dialog">
-		<div class="modal-content">
-			<div class="modal-header">
-				<button type="button" class="close novalidate" data-dismiss="modal">×</button>
-				<h3 class="modal-title"><?php echo $params['title']; ?></h3>
-			</div>
-			<div class="modal-body">
-				<?php echo $body; ?>
-			</div>
-			<div class="modal-footer">
-				<?php echo $params['footer']; ?>
-			</div>
-		</div>
+<div id="<?php echo $selector; ?>" tabindex="-1" class="modal hide fade">
+	<div class="modal-header">
+		<button type="button" class="close novalidate" data-dismiss="modal">×</button>
+		<h3><?php echo $params['title']; ?></h3>
+	</div>
+	<div class="modal-body">
+		<?php echo $body; ?>
+	</div>
+	<div class="modal-footer">
+		<?php echo $params['footer']; ?>
 	</div>
 </div>
