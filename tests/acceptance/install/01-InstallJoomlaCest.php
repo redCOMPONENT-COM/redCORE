9b78cd0b39a542c64094ef344ad16643bed4eceb
Tito Alvarez <augustoalvarez@gmail.com>
2018-04-25 20:57:55 -0600

    Debug

--- tests/acceptance/install/01-InstallJoomlaCest.php
+++ tests/acceptance/install/01-InstallJoomlaCest.php
@@ -10,7 +10,7 @@ class InstallJoomlaCest
 {
 	public function installJoomla(\AcceptanceTester $I)
 	{
-		$I->amOnPage('/');
+		$I->amOnPage('/README.txt');
 		$I->waitForText('I will not find anything');
 		$I->wantToTest('Joomla 3 Installation');
 		$I->installJoomlaMultilingualSite();
