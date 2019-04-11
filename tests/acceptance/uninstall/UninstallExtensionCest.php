b11718650f389592d2c0901c8068a2565969aa32
nhung <nhung@redweb.dk>
2019-04-11 14:05:46 +0700

    add waitForElement

--- tests/acceptance/uninstall/UninstallExtensionCest.php
+++ tests/acceptance/uninstall/UninstallExtensionCest.php
@@ -28,7 +28,7 @@ class UninstallExtensionCest
 		$I->doAdministratorLogin();
 		$I->amOnPage('/administrator/index.php?option=com_installer&view=manage');
 		$I->click("//button[@class='btn hasTooltip js-stools-btn-filter']");
-		$I->wait(1);
+		$I->waitForElementVisible('#filter_type', 30);
 		$I->selectOptionInChosen('#filter_type', 'Component');
 		$I->fillField(['id' => 'filter_search'], 'redCORE');
 		$I->pressKey(['id' => 'filter_search'], WebDriverKeys::ENTER);
