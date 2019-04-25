5e48d0c576c8a8dab6ba5485301aa89050bf3d31
Thu Nguyen <thu.nguyen@redweb.dk>
2019-04-25 09:55:33 +0700

    Edit style

--- tests/_support/Page/ActivateWebServicesPage.php
+++ tests/_support/Page/ActivateWebServicesPage.php
@@ -15,129 +15,129 @@ namespace Page;
 class ActivateWebServicesPage
 {
 
-    /**
-     * @var string
-     */
-    public static $URL                    = 'administrator/index.php?option=com_redcore&view=config&layout=edit&component=com_redcore';
+	/**
+	 * @var string
+	 */
+	public static $URL                    = 'administrator/index.php?option=com_redcore&view=config&layout=edit&component=com_redcore';
 
-    /**
-     * @var string
-     */
-    public static $URL2                   = 'administrator/index.php?option=com_redcore&view=webservices';
+	/**
+	 * @var string
+	 */
+	public static $URL2                   = 'administrator/index.php?option=com_redcore&view=webservices';
 
-    /**
-     * @var string
-     */
-    public static $buttonWebservice       = 'Webservice options';
+	/**
+	 * @var string
+	 */
+	public static $buttonWebservice       = 'Webservice options';
 
-    /**
-     * @var array
-     */
-    public static $h1                     = ['css' => 'h1'];
+	/**
+	 * @var array
+	 */
+	public static $h1                     = ['css' => 'h1'];
 
-    /**
-     * @var string
-     */
-    public static $titleRedConf           = 'redCORE Config';
+	/**
+	 * @var string
+	 */
+	public static $titleRedConf           = 'redCORE Config';
 
-    /**
-     * @var array
-     */
-    public static $form                   = '#REDCORE_WEBSERVICES_OPTIONS';
+	/**
+	 * @var array
+	 */
+	public static $form                   = '#REDCORE_WEBSERVICES_OPTIONS';
 
-    /**
-     * @var string
-     */
-    public static $labelWebServices       = 'Enable webservices';
+	/**
+	 * @var string
+	 */
+	public static $labelWebServices       = 'Enable webservices';
 
-    /**
-     * @var string
-     */
-    public static $choose                 = 'Yes';
+	/**
+	 * @var string
+	 */
+	public static $choose                 = 'Yes';
 
-    /**
-     * @var string
-     */
-    public static $labelCheckUser         = 'Check user permission against';
+	/**
+	 * @var string
+	 */
+	public static $labelCheckUser         = 'Check user permission against';
 
-    /**
-     * @var string
-     */
-    public static $optional               = 'Joomla - Use already defined authorization checks in Joomla';
+	/**
+	 * @var string
+	 */
+	public static $optional               = 'Joomla - Use already defined authorization checks in Joomla';
 
-    /**
-     * @var string
-     */
-    public static $selectorFormScroll     = "#jform_enable_soap-lbl";
+	/**
+	 * @var string
+	 */
+	public static $selectorFormScroll     = "#jform_enable_soap-lbl";
 
-    /**
-     * @var string
-     */
-    public static $labelSOAP              = 'Enable SOAP Server';
+	/**
+	 * @var string
+	 */
+	public static $labelSOAP              = 'Enable SOAP Server';
 
-    /**
-     * @var string
-     */
-    public static $buttonSave             = 'Save';
+	/**
+	 * @var string
+	 */
+	public static $buttonSave             = 'Save';
 
-    /**
-     * @var string
-     */
-    public static $messageSaveSuccess     = 'Save success';
+	/**
+	 * @var string
+	 */
+	public static $messageSaveSuccess     = 'Save success';
 
-    /**
-     * @var array
-     */
-    public static $messageContainer       = '#system-message-container';
+	/**
+	 * @var array
+	 */
+	public static $messageContainer       = '#system-message-container';
 
-    /**
-     * @var string
-     */
-    public static $textManager            = 'Webservice Manager';
+	/**
+	 * @var string
+	 */
+	public static $textManager            = 'Webservice Manager';
 
-    /**
-     * @var string
-     */
-    public static $buttonNotInstall       = 'Not installed webservices ';
+	/**
+	 * @var string
+	 */
+	public static $buttonNotInstall       = 'Not installed webservices ';
 
-    /**
-     * @var array
-     */
-    public static $installElement         = '.lc-install_all_webservices';
+	/**
+	 * @var array
+	 */
+	public static $installElement         = '.lc-install_all_webservices';
 
-    /**
-     * @var string
-     */
-    public static $buttonInstall          = 'Install';
+	/**
+	 * @var string
+	 */
+	public static $buttonInstall          = 'Install';
 
-    /**
-     * @var string
-     */
-    public static $tableForm              = '//table[@id=\'oauthClientsList\']';
+	/**
+	 * @var string
+	 */
+	public static $tableForm              = '//table[@id=\'oauthClientsList\']';
 
-    /**
-     * @var string
-     */
-    public static $pathContactWebservice1 = 'administrator.contact.1.0.0.xml';
+	/**
+	 * @var string
+	 */
+	public static $pathContactWebservice1 = 'administrator.contact.1.0.0.xml';
 
-    /**
-     * @var string
-     */
-    public static $pathContactWebservice2 = 'site.contact.1.0.0.xml';
+	/**
+	 * @var string
+	 */
+	public static $pathContactWebservice2 = 'site.contact.1.0.0.xml';
 
-    /**
-     * @var string
-     */
-    public static $pathUserWebservice      = 'site.users.1.0.0.xml';
+	/**
+	 * @var string
+	 */
+	public static $pathUserWebservice      = 'site.users.1.0.0.xml';
 
-    /**
-     * @param $value
-     * @return string
-     */
-    public function returnXpath($value)
-    {
-        $xpath = "//span[contains(text(),'".$value."')]";
-        return $xpath;
-    }
+	/**
+	 * @param $value
+	 * @return string
+	 */
+	public function returnXpath($value)
+	{
+		$xpath = "//span[contains(text(),'".$value."')]";
+		return $xpath;
+	}
 
 }
--- tests/_support/Step/Acceptance/ActivateWebServicesSteps.php
+++ tests/_support/Step/Acceptance/ActivateWebServicesSteps.php
@@ -15,41 +15,41 @@ use Page\ActivateWebServicesPage as ActWebPage;
  */
 class ActivateWebServicesSteps extends \AcceptanceTester
 {
-    /**
-     * @throws \Exception
-     */
-    public function doActivateWebservices()
-    {
-        $I = $this;
-        $I->comment('I enable basic authentication');
-        $I->amOnPage(ActWebPage::$URL);
-        $I->waitForText(ActWebPage::$titleRedConf, 30, ActWebPage::$h1);
-        $I->click(ActWebPage::$buttonWebservice);
-        $I->waitForElementVisible(ActWebPage::$form);
-        $I->executeJS("javascript:document.getElementById(\"REDCORE_WEBSERVICES_OPTIONS\").scrollIntoView();");
-        $I->selectOptionInRadioField(ActWebPage::$labelWebServices, ActWebPage::$choose);
-        $I->selectOptionInChosen(ActWebPage::$labelCheckUser, ActWebPage::$optional);
-        $I->scrollTo(ActWebPage::$selectorFormScroll);
-        $I->selectOptionInRadioField(ActWebPage::$labelSOAP, ActWebPage::$choose);
-        $I->executeJS('window.scrollTo(0,0)');
-        $I->click(ActWebPage::$buttonSave);
-        $I->waitForText(ActWebPage::$messageSaveSuccess, 30, ActWebPage::$messageContainer);
-    }
+	/**
+	 * @throws \Exception
+	 */
+	public function doActivateWebservices()
+	{
+		$I = $this;
+		$I->comment('I enable basic authentication');
+		$I->amOnPage(ActWebPage::$URL);
+		$I->waitForText(ActWebPage::$titleRedConf, 30, ActWebPage::$h1);
+		$I->click(ActWebPage::$buttonWebservice);
+		$I->waitForElementVisible(ActWebPage::$form);
+		$I->executeJS("javascript:document.getElementById(\"REDCORE_WEBSERVICES_OPTIONS\").scrollIntoView();");
+		$I->selectOptionInRadioField(ActWebPage::$labelWebServices, ActWebPage::$choose);
+		$I->selectOptionInChosen(ActWebPage::$labelCheckUser, ActWebPage::$optional);
+		$I->scrollTo(ActWebPage::$selectorFormScroll);
+		$I->selectOptionInRadioField(ActWebPage::$labelSOAP, ActWebPage::$choose);
+		$I->executeJS('window.scrollTo(0,0)');
+		$I->click(ActWebPage::$buttonSave);
+		$I->waitForText(ActWebPage::$messageSaveSuccess, 30, ActWebPage::$messageContainer);
+	}
 
-    /**
-     * @throws \Exception
-     */
-    public function doWebserviceManager(){
-        $I= $this;
-        $I->amOnPage(ActWebPage::$URL2);
-        $I->waitForText(ActWebPage::$textManager, 30, ActWebPage::$h1);
-        $I->click(ActWebPage::$buttonNotInstall);
-        $I->waitForElement(ActWebPage::$installElement, 30);
-        $I->executeJS("javascript:window.scrollBy(0,200);");
-        $I->click(ActWebPage::$buttonInstall);
-        $I->waitForElement(ActWebPage::$tableForm, 60);
-        $I->waitForText(ActWebPage::$pathContactWebservice1, 30, ActWebPage::returnXpath(ActWebPage::$pathContactWebservice1));
-        $I->waitForText(ActWebPage::$pathContactWebservice2,30 , ActWebPage::returnXpath(ActWebPage::$pathContactWebservice2));
-        $I->waitForText(ActWebPage::$pathUserWebservice,30 , ActWebPage::returnXpath(ActWebPage::$pathUserWebservice));
-    }
+	/**
+	 * @throws \Exception
+	 */
+	public function doWebserviceManager(){
+		$I= $this;
+		$I->amOnPage(ActWebPage::$URL2);
+		$I->waitForText(ActWebPage::$textManager, 30, ActWebPage::$h1);
+		$I->click(ActWebPage::$buttonNotInstall);
+		$I->waitForElement(ActWebPage::$installElement, 30);
+		$I->executeJS("javascript:window.scrollBy(0,200);");
+		$I->click(ActWebPage::$buttonInstall);
+		$I->waitForElement(ActWebPage::$tableForm, 60);
+		$I->waitForText(ActWebPage::$pathContactWebservice1, 30, ActWebPage::returnXpath(ActWebPage::$pathContactWebservice1));
+		$I->waitForText(ActWebPage::$pathContactWebservice2,30 , ActWebPage::returnXpath(ActWebPage::$pathContactWebservice2));
+		$I->waitForText(ActWebPage::$pathUserWebservice,30 , ActWebPage::returnXpath(ActWebPage::$pathUserWebservice));
+	}
 }
