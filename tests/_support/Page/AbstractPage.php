3754708838d352caa6eb3985da69fdf7eee30b8f
Oscar <oscar@redweb.dk>
2019-05-09 03:02:11 -0600

    Refactor for test case of InstallJoomlaCest

--- tests/_support/Page/AbstractPage.php
+++ tests/_support/Page/AbstractPage.php
@@ -21,12 +21,37 @@ class AbstractPage
 	/**
 	 * @var string
 	 */
+	public static $clientId = '#client_id';
+
+	/**
+	 * @var string
+	 */
 	public static $buttonSave = 'Save';
+
+	/**
+	 * @var string
+	 */
+	public static $buttonSaveAndClose = 'Save & Close';
 
 	/**
 	 * @var array
 	 */
 	public static $messageContainer = '#system-message-container';
+
+	/**
+	 * @var array
+	 */
+	public static $sysMessageContainer = ['id' => 'system-message-container'];
+
+	/**
+	 * @var array
+	 */
+	public static $labelModulePosition = ['css' => "label[data-original-title='Status Module Position']"];
+
+	/**
+	 * @var array
+	 */
+	public static $linkAdvanced = ['link' => 'Advanced'];
 
 	/**
 	 * @var string
@@ -37,6 +62,46 @@ class AbstractPage
 	 * @var string
 	 */
 	public static $buttonInstall = 'Install';
+
+	/**
+	 * @var string
+	 */
+	public static $chooseNo = 'No';
+
+	/**
+	 * @var string
+	 */
+	public static $chooseYes = 'Yes';
+
+	/**
+	 * @var string
+	 */
+	public static $amAdmin = 'administrator';
+
+	/**
+	 * @var string
+	 */
+	public static $labelAdmin = 'Administrator';
+
+	/**
+	 * @var string
+	 */
+	public static $buttonExtensions = 'Extensions';
+
+	/**
+	 * @var string
+	 */
+	public static $buttonTemplates = 'Templates';
+
+	/**
+	 * @var string
+	 */
+	public static $statusModulePosition = 'Status Module Position';
+
+	/**
+	 * @var string
+	 */
+	public static $positionTop = 'Top';
 
 	/**
 	 * @param $value
---
+++ tests/_support/Step/Acceptance/TemplateSteps.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * @package     redCORE
+ * @subpackage  Step
+ * @copyright   Copyright (C) 2008 - 2019 redCOMPONENT.com. All rights reserved.
+ * @licence     GNU General Public License version 2 or later; see LICENSE.TXT
+ */
+namespace Step\Acceptance;
+
+use Page\TemplatePage as disableToolbars;
+
+/**
+ * Class TemplateSteps
+ * @package Step\Acceptance
+ */
+class TemplateSteps extends \AcceptanceTester
+{
+	/**
+	 * @throws \Exception
+	 */
+	public function disableTemplateFloatingToolbars()
+	{
+		$I = $this;
+		$I->comment('I disable the template floating toolbars');
+		$I->am(disableToolbars::$amAdmin);
+		$I->wantTo('disable the floating template toolbars');
+		$I->doAdministratorLogin();
+		$I->amOnPage(disableToolbars::$urlAdmin);
+		$I->waitForText(disableToolbars::$cPanel, 60, disableToolbars::$h1);
+		$I->click(disableToolbars::$buttonExtensions);
+		$I->waitForElement(disableToolbars::$templatesElement, 60);
+		$I->click(disableToolbars::$buttonTemplates);
+		$I->waitForText(disableToolbars::$templatesStyles, 60, disableToolbars::$h1);
+		$I->selectOptionInChosen(disableToolbars::$clientId, disableToolbars::$labelAdmin);
+		$I->waitForText(disableToolbars::$templatesStylesAdmin, 60, disableToolbars::$h1);
+		$I->click(disableToolbars::$isisDefault);
+		$I->waitForText(disableToolbars::$templatesEditStyle, 60, disableToolbars::$h1);
+		$I->click(disableToolbars::$linkAdvanced);
+		$I->waitForElement(disableToolbars::$labelModulePosition, 60);
+		$I->executeJS("window.scrollTo(0, document.body.scrollHeight);");
+		$I->selectOptionInChosen(disableToolbars::$statusModulePosition, disableToolbars::$positionTop);
+		$I->selectOptionInRadioField(disableToolbars::$pinnedToolbar, disableToolbars::$chooseNo);
+		$I->click(disableToolbars::$buttonSaveAndClose);
+		$I->waitForText(disableToolbars::$templatesStyleSaved, 60, disableToolbars::$sysMessageContainer);
+		$I->see(disableToolbars::$templatesStyleSaved, disableToolbars::$sysMessageContainer);
+	}
+}
