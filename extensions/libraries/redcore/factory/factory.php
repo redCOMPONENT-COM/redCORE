3f646e8af4f2a12d609917989ccb84f279da9e48
Julien Vonthron <julien.vonthron@gmail.com>
2020-06-14 21:22:35 +0200

    Added dispatcher adapter

--- extensions/libraries/redcore/factory/factory.php
+++ extensions/libraries/redcore/factory/factory.php
@@ -28,17 +28,11 @@ final class RFactory extends JFactory
 	/**
 	 * Get the event dispatcher
 	 *
-	 * @return  JEventDispatcher
+	 * @return  RHelperDispatcher
 	 */
 	public static function getDispatcher()
 	{
-		if (!self::$dispatcher)
-		{
-			self::$dispatcher = version_compare(JVERSION, '3.0', 'lt') ?
-				JDispatcher::getInstance() : JEventDispatcher::getInstance();
-		}
-
-		return self::$dispatcher;
+		return RHelperDispatcher::getInstance();
 	}
 
 	/**
